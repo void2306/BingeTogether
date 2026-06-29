b26b5f97291aa2f34a13eba01dc95ad69f68a9cb
unknown <akshat.pandey5376@gmail.com>
2026-06-29 18:49:18 +0530

    psql settings added

--- src/main/java/com/sakshi/bingetogetherbackend/model/User.java
+++ src/main/java/com/sakshi/bingetogetherbackend/model/User.java
@@ -34,8 +34,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
