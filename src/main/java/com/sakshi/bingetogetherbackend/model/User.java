297de25d0e6f5b88950d8cdccd4257c485bf55a7
void2306 <ksakshi8106@gmail.com>
2026-06-30 12:42:24 +0530

    Resolved newly introduced User model conflicts

--- src/main/java/com/sakshi/bingetogetherbackend/model/User.java
+++ src/main/java/com/sakshi/bingetogetherbackend/model/User.java
@@ -1,34 +1,3 @@
-//package com.sakshi.bingetogetherbackend.model;
-//
-//public class User {
-//
-//    private String username;
-//    private String email;
-//
-//    public User() {
-//    }
-//
-//    public User(String username, String email) {
-//        this.username = username;
-//        this.email = email;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//}
 package com.sakshi.bingetogetherbackend.model;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -49,43 +18,22 @@ public class User {
     public User() {
     }
 
-    public User(Long id,
-                String username,
-                String email,
-                String password) {
-
+    public User(Long id, String username, String email, String password) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
     }
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
-    public String getUsername() {
-        return username;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {this.email = email;}
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
